2e0ce9eb2d36ea44a78cb001b587c79ebac4d742
Mykhailo74 <130218368+Mykhailo74@users.noreply.github.com>
2023-08-26 14:56:33 +0300

    Update task-03.js

--- js/task-03.js
+++ js/task-03.js
@@ -12,3 +12,14 @@ const images = [
     alt: 'Group of Horses Running',
   },
 ];
+
+const gallery = document.querySelector('.gallery');
+
+const galleryItems = ({ url, alt }) =>
+  `<li><img src="${url}" alt="${alt}" width = 300 height = auto></li>`;
+const galleryMarkup = images.reduce(
+  (acc, item) => acc + galleryItems(item),
+  []
+);
+
+gallery.insertAdjacentHTML('afterbegin', galleryMarkup);
